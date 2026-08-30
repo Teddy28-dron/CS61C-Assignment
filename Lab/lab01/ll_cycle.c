2de3fd5e7af66afeb541851db0cd274dcc3c1985
yikangli <leeyk1934@gamil.com>
2026-08-30 20:03:14 +0800

    Finish Lab01

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -2,6 +2,13 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
-    return 0;
+    node *tortoise, *hare;
+    tortoise = head;
+    hare = head;
+    while(1){ // 此处根本无需判断终止条件，因为循环内部一定会有return
+        if(!hare||!hare->next) return 0;
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+        if(hare == tortoise) return 1;
+    }
 }

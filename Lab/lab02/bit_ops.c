05f80c4b54aa56eb96a23275ab2acbe45ca666f7
yikangli <leeyk1934@gamil.com>
2026-08-31 22:55:15 +0800

    Finish bit_ops and lfsr successfully

--- Lab/lab02/bit_ops.c
+++ Lab/lab02/bit_ops.c
@@ -3,25 +3,27 @@
 
 // Return the nth bit of x.
 // Assume 0 <= n <= 31
+// 思路：只需要让1与指定的位置做&运算即可提取该位
 unsigned get_bit(unsigned x,
                  unsigned n) {
-    // YOUR CODE HERE
-    // Returning -1 is a placeholder (it makes
-    // no sense, because get_bit only returns 
-    // 0 or 1)
-    return -1;
+    x = x>>n;
+    unsigned temp = x&1;
+    return temp;
 }
 // Set the nth bit of the value of x to v.
 // Assume 0 <= n <= 31, and v is 0 or 1
+// 思路：先将要改变的位置变成0（其余位置不变）（用mask），然后再将目标位置与0做|
 void set_bit(unsigned * x,
              unsigned n,
              unsigned v) {
-    // YOUR CODE HERE
+    *x = (*x)&(~(1u<<n));
+    *x = (*x)|(v<<n);
 }
 // Flip the nth bit of the value of x.
 // Assume 0 <= n <= 31
+// 思路：用XOR运算即可（0^1 = 1, 1^1 = 0，相同是0，不同是1)
 void flip_bit(unsigned * x,
               unsigned n) {
-    // YOUR CODE HERE
+    *x = (*x)^(1u<<n);
 }
 
--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -4,7 +4,19 @@
 #include <string.h>
 #include "lfsr.h"
 
+// 这里需要注意，由于是无符号数字向右移动，因此会自动补0，没必要用一个mask再变成0了
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    
+    uint16_t p0 = ((*reg)>>0)&1u;
+    uint16_t p2 = ((*reg)>>2)&1u;
+    uint16_t p3 = ((*reg)>>3)&1u;
+    uint16_t p5 = ((*reg)>>5)&1u;
+    uint16_t new = p0^p2^p3^p5;
+    *reg = (*reg)>>1u;
+    // uint16_t mask = ~(1u<<15);
+    new = new<<15;
+    // *reg = mask&(*reg);
+    *reg = new|(*reg);
+
 }
 
